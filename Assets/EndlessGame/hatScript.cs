e3c41e7c7108321742a1fc761b9bafd7d115bab3
scentedPancakes <awat.oliver@gmail.com>
2014-09-19 15:17:15 -0400

    Added iTween

--- Assets/EndlessGame/hatScript.cs
+++ Assets/EndlessGame/hatScript.cs
@@ -9,14 +9,15 @@ public class hatScript : MonoBehaviour
 	private bool dataSent;
 
 	private float delay;
-	endless_prototype controllerScript;
+//	endless_prototype controllerScript;
+	mainGame controllerScript;
 
 
 	// Use this for initialization
 	void Start () 
 	{
 		sceneController = GameObject.FindGameObjectWithTag("Scene Controller");
-		controllerScript = sceneController.GetComponent<endless_prototype>();
+		controllerScript = sceneController.GetComponent<mainGame>();
 	}
 	
 	// Update is called once per frame
@@ -31,12 +32,15 @@ public class hatScript : MonoBehaviour
 		{
 			if(!dataSent)
 			{
-				if(this.gameObject.transform.position.y > -4.0f)
+				if(this.gameObject.transform.position.y > controllerScript.mainCamera.transform.position.y - 4.0f)
 				{
 					Debug.Log ("Score++");
 					controllerScript.score++;
+					controllerScript.MoveCamera(this.gameObject.transform.localScale.y);
+
 					this.gameObject.rigidbody.isKinematic = true;
 					this.gameObject.transform.parent = GameObject.FindGameObjectWithTag("Head").transform; 
+
 					dataSent = true;
 				}
 				else
---
+++ Assets/EndlessGame/mainGame.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainGame : MonoBehaviour 
+{
+	public GameObject mainCamera;
+
+	//Here are balloons and everything associated with them
+	public GameObject [] balloonObject; 
+	public float [] balloonSpeed;
+	public float [] balloonLift;
+
+	public int waveNumber;
+	public bool waveSpawned;
+	public float timeBetweenWaves;
+	
+	public GameObject [] placeholderHat;
+	public GameObject poppedHatObject;
+
+	public GameObject headObject;
+
+	public int score;
+	public int health;
+
+	private bool headSwayDir;
+	private bool gameOver;
+
+	private Vector3 camStartPos;
+    
+	// Use this for initialization
+	void Start () 
+	{
+		gameOver = false;
+
+		score = 0;
+		health = 3;
+
+		waveNumber = 0;
+		timeBetweenWaves = 0.8f;   
+
+		camStartPos = mainCamera.transform.position;
+
+		if(Random.Range (0, 1.0f) > 0.5f)
+		{
+			headSwayDir = true;
+        }        
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+		if(health <= 0)
+		{
+			gameOver = true;
+		}
+		if(score > 1)
+		{
+            SwayHead();
+        }
+
+    }
+
+	void SpawnHat()
+	{
+
+	}
+
+	void SwayHead()
+	{
+		if(headObject.transform.position.x <= -3.2f * (float)score/30.0f)
+		{
+			headSwayDir = true;
+		}
+		if(headObject.transform.position.x >= 3.2f * (float)score/30.0f)
+		{
+			headSwayDir = false;
+		}
+		
+		if(headSwayDir)
+		{
+			headObject.transform.position = new Vector3(headObject.transform.position.x + Time.deltaTime*score*.1f,
+			                                            headObject.transform.position.y,
+			                                            headObject.transform.position.z);
+		}
+		else
+		{
+			headObject.transform.position = new Vector3(headObject.transform.position.x - Time.deltaTime*score*.1f,
+			                                            headObject.transform.position.y,
+                                                        headObject.transform.position.z);
+        }
+    }
+
+	public void MoveCamera(float _hatHeight)
+	{
+		iTween.MoveUpdate(mainCamera, new Vector3(mainCamera.transform.position.x,
+		                                          mainCamera.transform.position.y + _hatHeight,
+		                                          mainCamera.transform.position.z), 0.6f);
+	}
+}
